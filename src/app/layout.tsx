0583839e981c0436473c7196a4bf28f210db68bb
HajMatin <matinzamanide@gmail.com>
2026-01-04 00:40:43 +0330

    feat: add metadata (title and description) to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,16 @@ import "./globals.css";
 import Layout from "@/components/Layout/Layout";
 import AuthContextProvider from "@/context/AuthContext";
 import ChatBot from "@/components/ChatBot/ChatBot";
+import { Metadata } from "next"; // وارد کردن تایپ Metadata
+
+// ✅ اضافه کردن متادیتا
+export const metadata: Metadata = {
+  title: "نام فروشگاه شما | خرید انواع پمپ و لوازم صنعتی",
+  description: "بهترین قیمت خرید انواع پمپ‌های خانگی و صنعتی با ضمانت کیفیت در اپیکا.",
+  icons: {
+    icon: "https://apika.ir/images/apika2.svg", // مسیر لوگوی کوچک سایت
+  },
+};
 
 export default function RootLayout({
   children,
@@ -13,9 +23,12 @@ export default function RootLayout({
     <html lang="fa" dir="rtl">
       <body>
         <AuthContextProvider>
-        <ShoppingCartContextProvider>
-          <Layout>{children} <ChatBot/></Layout>
-        </ShoppingCartContextProvider>
+          <ShoppingCartContextProvider>
+            <Layout>
+              {children} 
+              <ChatBot/>
+            </Layout>
+          </ShoppingCartContextProvider>
         </AuthContextProvider>
       </body>
     </html>
